73561f8d1344f70a0fb5abb52ef7a9f747c1e216
Arindom Kundu <arindomkundu18@gmail.com>
2021-04-09 13:09:00 +0600

    Parentshis check

--- Basic Cpp/VectorTest.cpp
+++ Basic Cpp/VectorTest.cpp
@@ -1,7 +1,26 @@
-#include<bits\stdc++.h>
+#include <bits\stdc++.h>
 using namespace std;
 int main()
 {
-    cout<<"This Is edited from Remote Branch"<<endl;
+    stack<char> s;
+    string str;
+    cin >> str;
+    s.push(str[0]);
+    for (int i = 1; i < str.size(); i++)
+    {
+        if((s.top() == '(' && str[i] == ')') || (s.top() == '{' && str[i] == '}') || (s.top() == '[' && str[i] == ']') )
+        {
+            s.pop();
+            cout<<"Hello "<<endl;
+        }
+        else
+            s.push(str[i]);
+    }
+
+    if (s.empty())
+        cout << "Balanced" << endl;
+    else
+        cout << "Not Balanced" << endl;
+
     return 0;
 }
